989a20b5341bbcbd1503996e5fd143b28b676566
Ashish Krishnan <ashishkrish09@gmail.com>
2018-05-27 20:12:05 +0530

    Generate data classes.

--- src/main/kotlin/SampleGenerator.kt
+++ src/main/kotlin/SampleGenerator.kt
@@ -2,6 +2,7 @@ import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.ParameterSpec
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 import java.io.File
@@ -17,33 +18,58 @@ fun main(args: Array<String>) {
     val outFileName    = "Sample"
     val className      = ClassName(packageName = outPackageName, simpleName = outFileName)
 
-    FileSpec
-        .builder(outPackageName, outFileName)
-        .addComment("This should be replaced by custom copyright files")
-        .addType(TypeSpec.classBuilder(className)
-            .primaryConstructor(FunSpec
-                .constructorBuilder()
-                .addParameter("name", String::class)
-                .build()
-            )
-            .addProperty(PropertySpec
-                .builder("name", String::class)
-                .initializer("name")
-                .build()
-            )
-            .addFunction(FunSpec
-                .builder("prettyPrint")
-                .addStatement("println(%S)", "\$name")
-                .build()
-            )
-            .build()
-        )
-        .addFunction(FunSpec
-            .builder("main")
-            .addParameter("args", String::class, KModifier.VARARG)
-            .addStatement("Sample(\"Hello, \\nWorld\").prettyPrint()")
-            .build()
-        )
-        .build()
-        .writeTo(OUTPUT_DIR)
+  val sampleClassTypeSpec = TypeSpec.classBuilder(className)
+      .primaryConstructor(FunSpec
+          .constructorBuilder()
+          .addParameter("name", String::class)
+          .build()
+      )
+      .addProperty(PropertySpec
+          .builder("name", String::class)
+          .initializer("name")
+          .build()
+      )
+      .addFunction(FunSpec
+          .builder("prettyPrint")
+          .addStatement("println(%S)", "\$name")
+          .build()
+      )
+      .build()
+
+  val mainFunSpec = FunSpec
+      .builder("main")
+      .addParameter("args", String::class, KModifier.VARARG)
+      .addStatement("Sample(\"Hello, \\nWorld\").prettyPrint()")
+      .build()
+
+  val personDataClassTypeSpec = TypeSpec
+      .classBuilder("Person")
+      .addModifiers(KModifier.DATA)
+      .addProperty(
+          PropertySpec
+              .builder("age", Int::class)
+              .initializer("age")
+              .mutable(false)
+              .build()
+      )
+      .primaryConstructor(
+          FunSpec
+              .constructorBuilder()
+              .addParameter(
+                  ParameterSpec
+                      .builder("age", Int::class)
+                      .build()
+              )
+              .build()
+      )
+      .build()
+
+  FileSpec
+      .builder(outPackageName, outFileName)
+      .addComment("This should be replaced by custom copyright files")
+      .addType(sampleClassTypeSpec)
+      .addType(personDataClassTypeSpec)
+      .addFunction(mainFunSpec)
+      .build()
+      .writeTo(OUTPUT_DIR)
 }
